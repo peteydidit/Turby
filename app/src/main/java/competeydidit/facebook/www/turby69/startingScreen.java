9cdb288eb66c31437a196ff9e9c7f177fd265681
Peter Gibson <peteydidit@hotmail.com>
2015-02-12 21:16:12 -0500

    must follow directions in google maps api

--- app/src/main/java/competeydidit/facebook/www/turby69/startingScreen.java
+++ app/src/main/java/competeydidit/facebook/www/turby69/startingScreen.java
@@ -4,16 +4,98 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
 
 public class startingScreen extends ActionBarActivity {
 
+    private static final String TAG = "myMessage";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting_screen);
+        Log.i(TAG, "onCreate");
+        Button start_scout = (Button)findViewById(R.id.start_scout); //starting screen scout button
+        start_scout.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        //TextView titleText = (TextView)findViewById(R.id.titleText);
+                        //titleText.setText("scouting nearby areas");
+                        Intent myIntent = new Intent(startingScreen.this, MapsActivity.class);
+                        startingScreen.this.startActivity(myIntent);
+                    }
+                }
+        );
+        Button start_settings = (Button)findViewById(R.id.start_settings); //starting screen scout button
+        start_settings.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        //TextView titleText = (TextView)findViewById(R.id.titleText);
+                        //titleText.setText("scouting nearby areas");
+                        Intent myIntent = new Intent(startingScreen.this, SettingsActivity.class);
+                        startingScreen.this.startActivity(myIntent);
+                    }
+                }
+        );
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+    }
+
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(TAG, "onRestoreInstanceState");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
